c870aad7b44c36596399d3ea401b4baefa7c82b8
rezapratama10118055 <reza.10118055@mahasiswa.unikom.ac.id>
2021-07-01 20:45:14 +0700

    ketiga
    Signed-off-by: rezapratama10118055 <reza.10118055@mahasiswa.unikom.ac.id>

--- app/src/main/java/com/example/neurokids/myitem.java
+++ app/src/main/java/com/example/neurokids/myitem.java
@@ -18,22 +18,24 @@ public class myitem {
 
         };
         static String[] PenjelasanLengkap ={
-          "Merupakan sistem organ yang mengatur dan mengendalikan semua kegiatan aktivitas tubuh. Terdiri atas serabut saraf(Neuron) yang tersusun atas sel-sel saraf"
-           ,"Sistem saraf adalah sistem organ pada hewan yang terdiri atas serabut saraf yang tersusun atas sel-sel saraf yang saling terhubung dan esensial untuk persepsi sensoris indrawi, aktivitas motorik volunter dan involunter organ atau jaringan tubuh, dan homeostasis berbagai proses fisiologis tubuh."
-                ,"\nCerebum(Otak Besar)\nBerfungsi mengendalikan tubuh, gerak sadar seperti kontraksi otot, mengolah bahasa dan Berbicara bagian otak besar sebagai berikut\n"
-                +"\nLobus frontal\n"+
-                "Terletak di bagian depan dan berfungsi untuk mengatur cara berfikir, pemecahan masalah, pergerakan fisik, dan memori jangka pendek\n"
-                +"\nLobus periental\nTerletak dibagian tengah dan berfungsi untuk menafasirkan informasi sensorik seperti cita rasa, suhu, dan sentuhan.\n"
-                +"\nLobus okspital\nTerletak dibagian belakang dan berfungsi memperoses gambaran dari mata dan mengaitkan pada memori otak\n "
-                +"\nLobus temporal\n " +
-                "Terletak di bagian samping dan berfungsi memperoses informasi yang berhubungan dengan audiotori, dan memiliki peran penting dalam peyampaian memori\n"
-                +"\nCerebellum(Otak Kecil)\n" +
-                "Berfungsi memegang peranan penting dalam pergerakan, mengontrol keseimbangan tubuh, dan keterampilan motorik haws\n"
-                +"\nBrain Stem / Batang\n"+
-                "Otak terletak dibawah otak besar dan didepan otak kecil, menghubungkan otak ke sum sum tulang belakang dan mengontrol banyak fungsi terkait dengan detak jantung, Tekanan darah, dan pernapasan bagian brain stem / bagian otak : pons bagian terbesar batang otak, yang terlibat dalam koordinasi gerakan mata, wajah dan pendengaran.\n"
-                +"\nMid Brain / Otak Tengah\n"+
-                "Membantu memperoses informasi visual\n"
-                +"\nMedulla Oblongata\n"+
+          "\tMerupakan sistem organ yang mengatur dan mengendalikan semua kegiatan aktivitas tubuh. Terdiri atas serabut saraf(Neuron) yang tersusun atas sel-sel saraf"
+           ,"\tSistem saraf adalah sistem organ pada hewan yang terdiri atas serabut saraf yang tersusun atas sel-sel saraf yang saling terhubung dan esensial untuk persepsi sensoris indrawi, aktivitas motorik volunter dan involunter organ atau jaringan tubuh, dan homeostasis berbagai proses fisiologis tubuh."
+                ,"\nA.Cerebum(Otak Besar)\nBerfungsi mengendalikan tubuh, gerak sadar seperti kontraksi otot, mengolah bahasa dan Berbicara bagian otak besar sebagai berikut:\n"
+                +"\n\t1.Lobus frontal\n"+
+                "Terletak di bagian depan dan berfungsi untuk mengatur cara berfikir, pemecahan masalah, pergerakan fisik, dan memori jangka pendek.\n"
+                +"\n\t2.Lobus periental\nTerletak dibagian tengah dan berfungsi untuk menafasirkan informasi sensorik seperti cita rasa, suhu, dan sentuhan.\n"
+                +"\n\t3.Lobus okspital\nTerletak dibagian belakang dan berfungsi memperoses gambaran dari mata dan mengaitkan pada memori otak.\n "
+                +"\n\t4.Lobus temporal\n" +
+                "Terletak di bagian samping dan berfungsi memperoses informasi yang berhubungan dengan audiotori, dan memiliki peran penting dalam peyampaian memori.\n"
+                +"\nB.Cerebellum(Otak Kecil)\n" +
+                "Berfungsi memegang peranan penting dalam pergerakan, mengontrol keseimbangan tubuh, dan keterampilan motorik haws.\n"
+                +"\nC.Brain Stem / Batang\n"+
+                "Otak terletak dibawah otak besar dan didepan otak kecil, menghubungkan otak ke sum sum tulang belakang dan mengontrol banyak fungsi terkait dengan detak jantung, Tekanan darah, dan pernapasan bagian brain stem / bagian otak :\n"
+                +"\n\t1.Pons\n"+
+                "Bagian terbesar batang otak, yang terlibat dalam kordinat gerakan mata, wajah dan pendengaran.\n"
+                +"\n\t2.Mid Brain / Otak Tengah\n"+
+                "Membantu memperoses informasi visual.\n"
+                +"\n\t3.Medulla Oblongata\n"+
                 "Bertindak sebagai pusat pengendalian jantung dan paru paru dan juga mengatur fungsi bernapas, bersin dan menelan.\n"
 
         };
